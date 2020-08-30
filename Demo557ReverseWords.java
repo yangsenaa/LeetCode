fc16e67e98c80fbe6d50893e8817fdbff5cd454c
yangsenaa <30641560+yangsenaa@users.noreply.github.com>
2020-08-30 21:50:35 +0800

    Update Demo557ReverseWords.java

--- Demo557ReverseWords.java
+++ Demo557ReverseWords.java
@@ -6,12 +6,14 @@ class Solution {
         int p = -1;
         while(i<len){
             char c = s.charAt(i);
+            // 识别空格
             if(c == ' '){
                 for(int j=i-1;j>p;j--){
                     strbuf.append(s.charAt(j));
                 }
                 p = i;
                 strbuf.append(' ');
+            // 判断是否是最后一位
             }else if(i == len-1){
                 for(int j=i;j>p;j--){
                     strbuf.append(s.charAt(j));
